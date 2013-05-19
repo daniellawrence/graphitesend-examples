633dbe38a1242dadaf176370a7164aa06ceb68f9
daniellawrence <dannyla@linux.com>
2013-05-19 18:00:05 +1000

    Removed main-loop

--- proc_loadavg.py
+++ proc_loadavg.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
-import time
 import graphitesend
 
-
-g = graphitesend.init(group='loadavg_', suffix='min')
-
-while True:
-    (la1, la5, la15) = open('/proc/loadavg').read().strip().split()[:3]
-    print g.send_dict({'1': la1, '5': la5, '15': la15})
-    time.sleep(1)
+g = graphitesend.init(group='loadavg_', suffix='min', dryrun=True)
+(la1, la5, la15) = open('/proc/loadavg').read().strip().split()[:3]
+print g.send_dict({'1': la1, '5': la5, '15': la15})
